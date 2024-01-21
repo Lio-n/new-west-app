9d61ff743ea2718215ecc1e2c5703e1d843c60c4
Lion <leonardofontan14@gmail.com>
2024-01-21 16:10:00 -0300

    feat(tailwind):custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "body-400": "#7A7A7A",
+        "body-500": "#222",
+        "body-900": "#050315",
+        "chinese-blue": "#0D0C22",
+        "blueberry-600": "#4D1CED",
+        "blueberry-700": "#360FB8",
+        "blueberry-800": "#240A7B",
+      },
+    },
     fontFamily: {},
   },
-  safelist: [],
+  safelist: [
+    {
+      pattern: /(bg|text|border)-(body-400|body-500|body-900|chinese-blue|blueberry-600|blueberry-700|blueberry-800)/,
+    },
+  ],
   plugins: [],
 };
